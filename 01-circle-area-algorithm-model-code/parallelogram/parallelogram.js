d1e0652af1562c4176f1830d4f04c95e1123d08f
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-10-10 07:24:49 +0300

    algorithm and flow chart of parallelogram area

---
+++ 01-circle-area-algorithm-model-code/parallelogram/parallelogram.js
@@ -0,0 +1,19 @@
+// calculate area of parallelogram
+
+/*
+  input: b & h
+    b => base of parallelogram
+    h => height of parallelogram
+
+  process: area = b * h
+
+  output: area
+*/
+
+function parallelogramArea(b, h) {
+  const area = b * h;
+  return area;
+}
+
+console.log(parallelogramArea(5, 3)); // 15
+console.log(parallelogramArea(4, 6)); // 24
